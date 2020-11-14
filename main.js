a9c1716f5f5d235102c36631c4ca2f0a941462a1
Walid Amriou <walidamriou@gmail.com>
2020-11-14 13:54:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -60,13 +60,3 @@ MongoClient.connect(url,
 
     }
 );
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    dbo.createCollection("customers", function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
-    });
-  });
